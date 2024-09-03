712d26b97319c8a45134a5f86047eebe45a6a9ee
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-09-03 21:09:14 +0100

    chore: final setup.

---
+++ src/app/app.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { Props } from "@/interfaces/props";
+
+export default function App({ children }: Props) {
+    return (
+        <html lang="en">
+            <head>
+                <meta name="viewport" content="width=1024px, initial-scale=1.0"/>
+                <link rel="icon" href="/images/yard.png"/>
+            </head>
+            <body>{children}</body>
+        </html>
+    )
+}

9e200a560d90489a47d40f5a8ac8bd44d9254312
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-09-03 21:16:28 +0100

    chore: image exports.

---
+++ public/index.ts
@@ -0,0 +1,28 @@
+// Images.
+import astronaut from "./images/astronaut.svg"
+import background from "./images/background.svg"
+import failure from "./images/failure.svg"
+import nftImage from "./images/nft-imgs.svg"
+import nftSwap from "./images/nft-swap.svg"
+import success from "./images/success.svg"
+
+// Chains.
+import arbitrum from "./images/arbitrum.png"
+import base from "./images/base.png"
+import bsc from "./images/bsc.png"
+import scroll from "./images/scroll.png"
+import sepolia from "./images/sepolia.png"
+
+export default {
+    astronaut,
+    background,
+    failure,
+    nftImage,
+    nftSwap,
+    success,
+    arbitrum,
+    base,
+    bsc,
+    scroll,
+    sepolia
+}

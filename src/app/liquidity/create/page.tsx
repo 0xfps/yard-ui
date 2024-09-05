51e9dbb4004bcc4bb2c849085e58492fb97286aa
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-09-05 10:13:32 +0100

    chore: setup pages.

---
+++ src/app/liquidity/create/page.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function CreatePair() {
+    return (<>Create Pair.</>)
+}
---
+++ src/app/liquidity/page.tsx
@@ -0,0 +1,5 @@
+"use client"
+
+export default function AddLiquidity() {
+    return (<>Add Liquidity.</>)
+}

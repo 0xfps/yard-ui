f88580e93b857c99c1e4875d5f5df0f92f13f978
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-09-17 07:48:35 +0100

    chore: remove `GoldRush` SDK and disable `lint` rules.

--- src/utils/fetchers/get-nfts-owned-by-address.ts
+++ src/utils/fetchers/get-nfts-owned-by-address.ts
@@ -1,14 +1,11 @@
 import { SupportedChains } from "@/types/supported-chains";
-import { NftAddressBalanceNftResponse } from "@covalenthq/client-sdk";
-import { initGoldRushClient } from "../clients/covalent-client";
+import { initClient } from "../clients/client";
 
 export async function getNFTsOwnedByAddress(
     address: string,
     chain: SupportedChains
-): Promise<NftAddressBalanceNftResponse | undefined> {
-    const client = initGoldRushClient()
+) {
+    const client = initClient()
     if (!client) return undefined
-    const { error, data } = await client.NftService.getNftsForAddress(chain, address)
-    if (error || !data) return undefined
-    return data
+    return
 }
--- src/utils/is-supported-chain.ts
+++ src/utils/is-supported-chain.ts
@@ -1,5 +1,6 @@
 import { SUPPORTED_CHAIN_IDS } from "./constants";
 
-export function isSupportedChain(chainId: number): boolean {
+export function isSupportedChain(chainId: number | undefined): boolean {
+    if (!chainId) return false
     return SUPPORTED_CHAIN_IDS.includes(chainId)
 }

10c585acc17413cf150f84174dccbbfd52e63c6e
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-09-19 12:32:34 +0100

    feat: add `AOS` for animations.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -7,7 +7,7 @@ export default function App({ children }: Props) {
         <html lang="en">
             <head>
                 <meta name="viewport" content="width=1024px, initial-scale=1.0"/>
-                <link rel="icon" href="/images/yard.png"/>
+                <link rel="icon" href="/images/yard.png" />
             </head>
             <body>{children}</body>
         </html>

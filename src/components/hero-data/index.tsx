d4e67b3b6a3c21690a0b8e302c3dc591e7bd2238
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-10-09 08:50:52 +0100

    fix: update page data.

--- src/components/hero-data/index.tsx
+++ src/components/hero-data/index.tsx
@@ -5,16 +5,16 @@ export default function HeroData() {
         <div className="text-center mt-[90px]">
             <div className="flex justify-between items-center text-text w-[60%] m-auto">
                 <div>
-                    <span className="text-[60px] font-sf-bold">12+</span><br /><span className="text-[25px] font-sf-light">Pools</span>
+                    <span className="text-[60px] font-sf-bold">40+</span><br /><span className="text-[25px] font-sf-light">Pools</span>
                 </div>
                 <div>
-                    <span className="text-[60px] font-sf-bold">5+</span><br /><span className="text-[25px] font-sf-light">Collections</span>
+                    <span className="text-[60px] font-sf-bold">20+</span><br /><span className="text-[25px] font-sf-light">Collections</span>
                 </div>
                 <div>
                     <span className="text-[60px] font-sf-bold">20+</span><br /><span className="text-[25px] font-sf-light">Swaps</span>
                 </div>
                 <div>
-                    <span className="text-[60px] font-sf-bold">$80+</span><br /><span className="text-[25px] font-sf-light">Total Fees</span>
+                    <span className="text-[60px] font-sf-bold">$8+</span><br /><span className="text-[25px] font-sf-light">Total Fees</span>
                 </div>
             </div>
         </div>

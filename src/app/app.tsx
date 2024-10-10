9ece4e784acef415a176afd264c0930dd8867e44
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-10-10 08:55:36 +0100

    restrict view to big screens.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,31 @@
 "use client"
 
+import AppContainer from "@/components/app-container";
+import useWindowDimensions from "@/hooks/useWindowDimensions";
 import { Props } from "@/interfaces/props";
 
 export default function App({ children }: Props) {
+    const { width } = useWindowDimensions()
+
     return (
         <html lang="en">
             <head>
-                <meta name="viewport" content="width=800, initial-scale=0.2"/>
+                <meta name="viewport" content="width=800, initial-scale=0.2" />
                 <link rel="icon" href="/images/yard.png" />
             </head>
-            <body>{children}</body>
+            <body>
+                {
+                    width != 0 ?
+                        (width > 800)
+                            ? children
+                            : <AppContainer>
+                                <div className="w-full h-full flex justify-center items-center text-3xl font-sf-bold">
+                                    Yard is not supported on small screens.
+                                </div>
+                            </AppContainer>
+                        : <></>
+                }
+            </body>
         </html>
     )
 }

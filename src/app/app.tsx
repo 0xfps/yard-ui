46786f560e9ff8caddbab4a6e5a89ebb2a55acf8
0xfps <anthony.nnaemeka.umeh@gmail.com>
2024-10-10 10:08:01 +0100

    fix: proper layout.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,31 +1,20 @@
 "use client"
 
-import AppContainer from "@/components/app-container";
-import useWindowDimensions from "@/hooks/useWindowDimensions";
+import { WagmiProvider } from "wagmi";
+import "./globals.css";
+import { wagmiConfig } from "../../public/config/wagmi-config";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Modals from "@/components/modals";
 import { Props } from "@/interfaces/props";
+const queryClient = new QueryClient()
 
 export default function App({ children }: Props) {
-    const { width } = useWindowDimensions()
-
     return (
-        <html lang="en">
-            <head>
-                <meta name="viewport" content="width=800, initial-scale=0.2" />
-                <link rel="icon" href="/images/yard.png" />
-            </head>
-            <body>
-                {
-                    width != 0 ?
-                        (width > 800)
-                            ? children
-                            : <AppContainer>
-                                <div className="w-full h-full flex justify-center items-center text-3xl font-sf-bold">
-                                    Yard is not supported on small screens.
-                                </div>
-                            </AppContainer>
-                        : <></>
-                }
-            </body>
-        </html>
+        <WagmiProvider config={wagmiConfig}>
+            <QueryClientProvider client={queryClient}>
+                <Modals />
+                {children}
+            </QueryClientProvider>
+        </WagmiProvider>
     )
 }
